693e235d307adee31f6121fe0876d95f64782561
Erikqtx <glaser-erik@web.de>
2021-05-19 15:43:37 +0200

    Home-Screen, Links missing

--- quickportfolio/src/components/sidebar.js
+++ quickportfolio/src/components/sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import * as FaIcons from "react-icons/fa";
 import * as MdIcons from "react-icons/md";
 import * as BiIcons from "react-icons/bi";
--- quickportfolio/src/pages/home.js
+++ quickportfolio/src/pages/home.js
@@ -2,10 +2,12 @@ import React, { useEffect } from "react";
 import Erik from "../Erik.jpg";
 import Typical from "react-typical";
 import "./home.css";
+import ParticlesBg from "particles-bg";
 
 function Home() {
   return (
     <div className="home">
+      <ParticlesBg type="circle" bg={true} />
       <div className="bg-white max-w-md mx-auto rounded-xl shadow-2xl overflow-hidden md:max-w-screen-lg p-2">
         <img
           src={Erik}
@@ -32,12 +34,77 @@ function Home() {
                 1000,
               ]}
             />
-          </p>{" "}
+          </p>
           <p></p>
           <p className="info">
             Welcome to my quickly build Portfolio for the district town hall in
             Ansbach.
           </p>
+        </div>
+      </div>
+
+      <div class="max-w-screen-xl mx-auto px-4">
+        <div class="-mx-4 flex flex-wrap">
+          <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/3">
+            <div class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg">
+              <div class="flex-1">
+                <h2 class="text-gray-900 text-2xl font-bold leading-snug">
+                  Amazon Clone
+                </h2>
+                <p class="mt-2 text-lg">
+                  Fully functional Amazon clone. Achieved with React.JS,
+                  MongoDB, Express.JS, Node.JS and Cloud/Real-Time Storage from
+                  Firebase.
+                </p>
+              </div>
+              <a
+                href="#"
+                class="mt-6 inline-flex items-center px-6 py-3 text-white font-semibold bg-blue-700 rounded-md shadow-sm"
+              >
+                View Project
+              </a>
+            </div>
+          </div>
+
+          <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/3">
+            <div class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg">
+              <div class="flex-1">
+                <h2 class="text-gray-900 text-2xl font-bold leading-snug">
+                  Whatsapp Clone
+                </h2>
+                <p class="mt-2 text-lg">
+                  Functional Whatsapp Desktop Clone. Achieved with React.JS,
+                  Firebase.
+                </p>
+              </div>
+              <a
+                href="#"
+                class="mt-6 inline-flex items-center px-6 py-3 text-white font-semibold bg-blue-700 rounded-md shadow-sm"
+              >
+                View project
+              </a>
+            </div>
+          </div>
+          <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/3">
+            <div class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg">
+              <div class="flex-1">
+                <h2 class="text-gray-900 text-2xl font-bold leading-snug">
+                  Cine.Mate - A Tinder-like App
+                </h2>
+                <p class="mt-2 text-lg">
+                  Cine.Mate a fully responsive, universal mobile/desktop
+                  application. Achieved with React Native, Firebase, Node.JS,
+                  MongoDB.
+                </p>
+              </div>
+              <a
+                href="#"
+                class="mt-6 inline-flex items-center px-6 py-3 text-white font-semibold bg-blue-700 rounded-md shadow-sm"
+              >
+                View project
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
